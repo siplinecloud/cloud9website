69056ee0c830a30b16b977761b7065b1bfe5699b
wiseroo <wisdommsema@gmail.com>
2024-07-28 02:25:34 +0200

    Requied validation.

--- src/app/demo-request/page.tsx
+++ src/app/demo-request/page.tsx
@@ -60,6 +60,7 @@ const RequestDemo = () => {
                       placeholder="Enter your Name"
                       className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                       disabled={isLoading}
+                      required
                     />
                   </div>
                   <div className="mb-8">
@@ -76,6 +77,7 @@ const RequestDemo = () => {
                       placeholder="Enter your Email"
                       className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                       disabled={isLoading}
+                      required
                     />
                   </div>
                   <input
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -62,6 +62,7 @@ const Contact = () => {
                         placeholder="Enter your name"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                         disabled={isLoading}
+                        required
                       />
                     </div>
                   </div>
@@ -80,6 +81,7 @@ const Contact = () => {
                         placeholder="Enter your email"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                         disabled={isLoading}
+                        required
                       />
                     </div>
                   </div>
@@ -98,6 +100,7 @@ const Contact = () => {
                         placeholder="Enter your Message"
                         className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                         disabled={isLoading}
+                        required
                       ></textarea>
                     </div>
                   </div>
